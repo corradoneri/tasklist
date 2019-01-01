78f22532a9d17225e6b0f7a1d8f9306dce4c0603
Corrado Neri <corry.neri@gmail.com>
2019-01-01 15:13:21 -0600

    Initial project structure setup

---
+++ Core/DataAccess/TaskRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Tasks.Core.Models;
+
+namespace Tasks.Core.DataAccess
+{
+    public class TaskRepository : ITaskRepository
+    {
+        public void AddRecord(Task task)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Task> GetTasks()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateTasks(Task task)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Core/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tasks.Core.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(5)]
+        [MaxLength(64)]
+        public string Title { get; set; }
+        [MaxLength(256)]
+        public string Description { get; set; }
+        public bool Completed { get; set; }
+    }
+}
